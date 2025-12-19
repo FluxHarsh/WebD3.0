53f5b9a282f84284dc79df33c7494f82c3ec8522
Harsh Jagtap <harshjagtap843@gmail.com>
2025-12-19 20:43:24 +0530

    Add Week4.1 Node.js CLI learning files

---
+++ Week3.1/index.js
@@ -0,0 +1,29 @@
+// function addTodo() {
+//     const inputEl = document.querySelector("input");
+//     console.log(inputEl.value);
+// }
+
+// let ctr = 0;
+// function timeWatch() {
+//     document.querySelectorAll("h4")[1].innerHTML = ctr;
+
+//     ctr = ctr + 1;
+//     console.log(ctr)
+//     setTimeout(timeWatch, 1000);
+// }
+
+// setTimeout(timeWatch, 1000)
+
+// //run this onClick
+// function deleteT(index){
+//     const element = document.getElementById("todo-"+ index );
+//     element.parentNode.removeChild(element)
+// }
+
+
+
+
+  function deleteTodo(index) {
+    const element = document.getElementById("todo-" + index);
+    element.parentNode.removeChild(element);
+  }
