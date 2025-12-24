22b51187706eb75552bf3fe9e68140d94fc86854
Harsh Jagtap <harshjagtap843@gmail.com>
2025-12-24 23:17:02 +0530

    hello

---
+++ Week5.1/index.js
@@ -0,0 +1,49 @@
+// hello world jai shree ram
+
+const express = require('express');
+const app =express();
+
+app.get('/multiply', function(req,res){
+    const a = req.query.a 
+    const b = req.query.b
+
+    res.json({
+        ans: a * b
+    })
+
+})
+
+
+app.get('/add', function(req,res){
+    const a = parseInt(req.query.a)
+    const b =  parseInt(req.query.b)
+
+    res.json({
+        ans: a +  b
+    })
+
+})
+
+
+app.get('/divide', function(req,res){
+    const a = req.query.a 
+    const b = req.query.b
+
+    res.json({
+        ans: a / b
+    })
+
+})
+
+
+app.get('/subtract', function(req,res){
+    const a = parseInt(req.query.a )
+    const b = parseInt(req.query.b)
+
+    res.json({
+        ans: a - b
+    })
+
+})
+
+app.listen(3000)
