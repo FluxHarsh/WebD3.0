3929b17e4f468b19874c646ff0adea237d6f0d00
Harsh Jagtap <harshjagtap843@gmail.com>
2025-12-27 18:42:35 +0530

    Update logic in index.js

--- Week5.2/index.js
+++ Week5.2/index.js
@@ -1,32 +1,57 @@
 const express = require('express')
 const app = express();
 
-let requestRecieved = 0;
+// let requestRecieved = 0;
 
 
-function trialMiddleware(req, res, next) {
+// function trialMiddleware(req, res, next) {
 
-    requestRecieved = requestRecieved+1
-    req.name = "realHarsh"
+//     requestRecieved = requestRecieved+1
+//     req.name = "realHarsh"
 
-    console.log( `Total number of requests recieved = ${requestRecieved}` )
+//     console.log( `Total number of requests recieved = ${requestRecieved}` )
+//     console.log("Method is "+ req.method )
+//     console.log("Host is "+ req.hostname + req.url )
+//     console.log("Route is "+ req.url )
+//     console.log(new Date())
 
-    next()
-}
+//     next()
+// }
 
-function realSumHandler(req,res){
+// function realSumHandler(req,res){
 
-    const aman = parseInt(req.query.a);
-    const baman = parseInt(req.query.b);
-    console.log(req.name)
+//     const aman = parseInt(req.query.a);
+//     const baman = parseInt(req.query.b);
+//     console.log(req.name)
+
+//     res.json({
+//         add: aman + baman
+//     })
+// }
+
+
+// app.get('/sum', trialMiddleware, realSumHandler )
+
+// app.listen(3000)
+ 
+
+
+//Built in midlleware- express.json() under the hood it's body-parser
+
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/sum', function(req,res){
+    const a = parseInt(req.body.a)
+    const b = parseInt(req.body.b)
+    console.log(req.body)
 
     res.json({
-        add: aman + baman
+        ans: a + b
     })
-}
+})
+// serves a folder over http
 
-
-app.get('/sum', trialMiddleware, realSumHandler )
-
-app.listen(3000)
- 
+app.listen(3000);
