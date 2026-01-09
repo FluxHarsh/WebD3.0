62720b228bea693a86cd6bacbac7f9dc4fb08d84
Harsh Jagtap <harshjagtap843@gmail.com>
2026-01-09 09:35:31 +0530

    context api

--- Week10.2/src/App.jsx
+++ Week10.2/src/App.jsx
@@ -1,42 +1,89 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import './App.css'
 
-function App() {
-  return <div>
-    <LightBlub/>
-  </div>
-}
+// function App() {
+//   return <div>
+//     <LightBlub/>
+//   </div>
+// }
 
-function LightBlub(){
-    const [bulbOn, setBlubOn]=useState(true)
+// function LightBlub(){
+//     const [bulbOn, setBlubOn]=useState(true)
 
-//bulbOn is a prop to the BlubState component
-//setBlubOn is a prop to the ToggleBulbState component
+// //bulbOn is a prop to the BlubState component
+// //setBlubOn is a prop to the ToggleBulbState component
 
-  return <div>
-    <BlubState bulbOn={bulbOn} />
-    <ToggleBulbState setBlubOn={setBlubOn} />
-  </div>
-}
+//   return <div>
+//     <BlubState bulbOn={bulbOn} />
+//     <ToggleBulbState setBlubOn={setBlubOn} />
+//   </div>
+// }
 
-function BlubState({bulbOn}){
+// function BlubState({bulbOn}){
 
-  return <div>
-    {bulbOn ? "Bulb on" : "Bulb off" }
+//   return <div>
+//     {bulbOn ? "Bulb on" : "Bulb off" }
     
-  </div>
+//   </div>
+// }
+
+// function ToggleBulbState({setBlubOn}){
+
+//   function toggle(){
+//     setBlubOn(currentState => !currentState)
+//   }
+
+//   return <div>
+//     <button onClick={toggle} >Toggle the button</button>
+//   </div>
+// }
+
+
+// export default App
+
+
+import React, { createContext, useContext, useState } from 'react';
+
+const CountContext = createContext();
+
+function CountContextProvider({ children }) {
+  const [count, setCount] = useState(0);
+
+  return <CountContext.Provider value={{count, setCount}}>
+    {children}
+  </CountContext.Provider>
 }
 
-function ToggleBulbState({setBlubOn}){
+function Parent() {
+  return (
+    <CountContextProvider>
+      <Increase />
+      <Decrease />
+      <Value />
+    </CountContextProvider>
+  );
+}
 
-  function toggle(){
-    setBlubOn(currentState => !currentState)
-  }
+function Decrease() {
+  const {setCount} = useContext(CountContext);
+  return <button onClick={() => setCount(count => count - 1)}>Decrease</button>;
+}
 
+function Increase() {
+  const {count, setCount} = useContext(CountContext);
+  return <button onClick={() => setCount(count + 1)}>Increase</button>;
+}
+
+function Value() {
+  const {count} = useContext(CountContext);
+  return <p>Count: {count}</p>;
+}
+
+// App Component
+const App = () => {
   return <div>
-    <button onClick={toggle} >Toggle the button</button>
+    <Parent />
   </div>
-}
+};
 
-
-export default App
+export default App;
