e131c7b75a17873a1cb61d256f6d5f5248ece97c
Harsh Jagtap <harshjagtap843@gmail.com>
2026-01-31 23:34:11 +0530

    typescript2

--- Week14.1/index.js
+++ Week14.1/index.js
@@ -2,4 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let x = 1;
 console.log(x);
+function greet(firstName) {
+    console.log("Hello " + firstName);
+}
+greet("Harsh");
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 9));
+function delayedCall(anotherFn) {
+    setTimeout(anotherFn, 2000);
+}
+function log() {
+    console.log('mY name is mr random');
+}
+delayedCall(log);
 //# sourceMappingURL=index.js.map
--- Week14.1/index.ts
+++ Week14.1/index.ts
@@ -2,3 +2,27 @@ let x : number = 1;
 
 console.log(x)
 
+
+function greet(firstName: string ){
+    console.log("Hello " + firstName )
+}
+
+
+greet("Harsh")
+
+
+function sum(a:number, b:number): number {
+    return a+b
+}
+
+console.log(sum(1,9))
+
+function delayedCall(anotherFn: () => void ){
+    setTimeout(anotherFn, 2000)
+}
+
+function log(){
+    console.log('mY name is mr random')
+}
+
+delayedCall(log)
