dc2e9557e687d48b0d358c5a974afa9b653e3206
Harsh Jagtap <harshjagtap843@gmail.com>
2026-06-12 15:41:06 +0530

    Week17.1 postgresql

---
+++ Week17.1/dist/index.js
@@ -0,0 +1,17 @@
+import { Client } from "pg";
+const pgClient = new Client("postgresql://neondb_owner:npg_gnLvXIwWU43d@ep-green-wind-ae5tvnyj-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password:"npg_gnLvXIwWU43d",
+//     port: 5432,
+//     host:"ep-green-wind-ae5tvnyj-pooler.c-2.us-east-2.aws.neon.tech",
+//     database: "neondb"
+// })
+async function main() {
+    await pgClient.connect();
+    console.log("Database connected successfully !!");
+    const response = await pgClient.query('SELECT * FROM usersnew');
+    console.log(response);
+}
+main();
+//# sourceMappingURL=index.js.map
